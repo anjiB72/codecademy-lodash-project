a621e72eeb20565b2f3ba7a817235ee9310e47fb
anjiB72 <43617582+anjiB72@users.noreply.github.com>
2019-07-28 18:21:47 +0100

    added has method to lodash object

--- _.js
+++ _.js
@@ -1,40 +1,44 @@
 var _ = {
-  clamp(number, lower, upper){
-      var lowerClampedValue = Math.max(number, lower);
-      var clampedValue = Math.min(lowerClampedValue, upper);
-      return clampedValue;
-  }, 
-  inRange(number, start, end){
-      if(end === undefined){
-          end = start;
-          start = 0;
-      }; 
-      if(start > end){
-          let temp = end;
-          end = start;
-          start = temp;        
-      };
-      var isInRange = (number >= start) && (number < end);
-      return isInRange;
-  },
-  words(string){
-      var words = string.split(' ');
-      return words;
-  },
-  pad(string, length){
-      if(length <= string.length){
-          return string;
-      } else {
-          var startPaddingLength = Math.floor((length - string.length)/2);
-          var endPaddingLength = length - startPaddingLength - string.length;
-          var space = ' ';
-          var paddedString = space.repeat(startPaddingLength) + string + space.repeat(endPaddingLength);
-          return paddedString;
-      }
-  },
-  
+    clamp(number, lower, upper) {
+        var lowerClampedValue = Math.max(number, lower);
+        var clampedValue = Math.min(lowerClampedValue, upper);
+        return clampedValue;
+    },
+    inRange(number, start, end) {
+        if (end === undefined) {
+            end = start;
+            start = 0;
+        };
+        if (start > end) {
+            let temp = end;
+            end = start;
+            start = temp;
+        };
+        var isInRange = (number >= start) && (number < end);
+        return isInRange;
+    },
+    words(string) {
+        var words = string.split(' ');
+        return words;
+    },
+    pad(string, length) {
+        if (length <= string.length) {
+            return string;
+        } else {
+            var startPaddingLength = Math.floor((length - string.length) / 2);
+            var endPaddingLength = length - startPaddingLength - string.length;
+            var space = ' ';
+            var paddedString = space.repeat(startPaddingLength) + string + space.repeat(endPaddingLength);
+            return paddedString;
+        }
+    },
+    has(object, key){
+        var hasValue = object[key];
+        if (hasValue != undefined){
+            return true;
+        } return false;
+    } 
 };
-
 
 
 
